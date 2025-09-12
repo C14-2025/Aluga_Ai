8a19ff691a0b4c64c274189c0ba3608a103324c4
AlvaroCareli <122116260+alvarosamp@users.noreply.github.com>
2025-09-11 22:51:46 -0300

    Adicionando testes unitários

--- API/TesteApi.py
+++ API/TesteApi.py
@@ -1,7 +1,15 @@
 import pytest
-from ChamadaApi import chamada_api_status
+from ChamadaApi import chamada_api_status, chamada_api
 
 def test_chamada_api_status():
     status = chamada_api_status()
     assert status == 200, f"Status retornado foi {status}, esperado 200"
+
+def test_chamada_api_retorna_string():
+    resposta = chamada_api()
+    assert isinstance(resposta, str), "A resposta não é uma string."
+
+def test_chamada_api_contem_school():
+    resposta = chamada_api()
+    assert "school" in resposta.lower(), "A resposta não contém o campo 'school'."
     
