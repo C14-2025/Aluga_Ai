81fac35a1a0b9c3106ea0e633a170c7742290695
Álvaro Careli <122116260+alvarosamp@users.noreply.github.com>
2025-10-09 17:09:04 -0300

    Revert "feat: implementar sistema completo de reservas de imóveis"

--- aluga_ai_web/aluga_ai_web/settings.py
+++ aluga_ai_web/aluga_ai_web/settings.py
@@ -40,10 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'django_filters',
     'usuarios',
-    'reservas',
 ]
 
 MIDDLEWARE = [
