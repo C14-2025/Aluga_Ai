bdcf990b67d237d218c2837dfa023f2b213e968a
D1D104 <diegoresende403@gmail.com>
2025-10-10 16:47:09 -0300

    Removida estrutura antiga duplicada em aluga_ai_web/ (configurações, apps e ML)

--- aluga_ai_web/ml/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PricePredictionView
-
-urlpatterns = [
-    path('price/', PricePredictionView.as_view(), name='ml-price'),
-]
-
--- reservas/views.py
+++ reservas/views.py
@@ -1,7 +1,21 @@
-from rest_framework import generics
-from .serializers import RegisterSerializer
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Reserva
+from propriedades.models import Propriedade
+from django.utils.dateparse import parse_date
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
+@login_required
+def nova_reserva(request, prop_id):
+    prop = get_object_or_404(Propriedade, id=prop_id)
+    if request.method == "POST":
+        inicio = parse_date(request.POST.get("inicio"))
+        fim = parse_date(request.POST.get("fim"))
+        # TODO: checar disponibilidade
+        Reserva.objects.create(guest=request.user, propriedade=prop, inicio=inicio, fim=fim)
+        return redirect("reservas:minhas")
+    return render(request, "reservas/nova.html", {"propriedade": prop})
+
+@login_required
+def minhas_reservas(request):
+    reservas = request.user.reservas_feitas.all()
+    return render(request, "reservas/minhas.html", {"reservas": reservas})
